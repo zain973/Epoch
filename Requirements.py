803573c36b52e51f6fdcce3d981485e7f49c4a43
zain973 <zainkhan.ibm@gmail.com>
2026-05-19 20:41:34 +0530

    Create Requirements.py

---
+++ Requirements.py
@@ -0,0 +1,14 @@
+# Core API Framework
+fastapi==0.111.0
+uvicorn[standard]==0.30.1
+
+# Data Validation & Serialization
+pydantic[email]==2.7.4
+
+# Security, Identity & Authentication Tokens
+passlib[bcrypt]==1.7.4
+pyjwt[crypto]==2.8.0
+python-multipart==0.0.9
+
+# Environment and Configuration Management
+python-dotenv==1.0.1
